a128d38cd3c19b8fb77abe1c45ef0c2bbaa69e74
William José Dias <william.dias@cnpem.br>
2021-10-18 13:34:36 -0300

    instalando sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-   <h1>Hello NextJS</h1>
+    <>
+      <h1 className={styles.title}>
+        Ignews
+        <span> Rocketsear</span>
+      </h1>
+
+    </>
   )
 }
